dbcef7c2cecb84ccfb9d6c7f40a0dce41c30cf4f
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-03-07 22:21:14 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+
+const greet = ref("Hello World")
+const testimonialdata = ref([{
+  user_name: " John Tarkpor",
+  user_role: "Junior Front-end Developer",
+  "user_testimonial": " If you want to lay the best foundation possible I’d recommend taking this course.\
+  The depth the instructors go into is incredible. I now feel so confident about \
+  starting up as a professional developer. "
+},
+{
+  user_name: "Tanya Sinclair",
+  user_role: "UX Engineer",
+  "user_testimonial": " I’ve been interested in coding for a while but never taken the jump, until now.\
+  I couldn’t recommend this course enough. I’m now in the job of my dreams and so\
+  excited about the future."
+},])
+
+</script>
+
+<template>
+  <h1>{{ greet }}</h1>
+</template>
+
+<style scoped></style>
---
+++ src/components/Silde.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, computed } from 'vue';
+
+const currentIndex = ref(0)
+const currentSlide = computed(() => slides.value[currentIndex.value])
+
+const nextSlide = () => {
+    currentIndex.value = (currentIndex.value + 1) % slides.value.length;
+}
+
+const previousSlide = () => {
+    currentIndex.value = (currentIndex.value - 1 + slide.value.length) % slide.value.lenght
+}
+</script>
+
+<template>
+    <div>
+        <a href="">
+            <img src="./images/icon-next.svg" alt="">
+            <img src="./images/icon-prev.svg" alt="">
+        </a>
+        <a href=""></a>
+    </div>
+</template>

f245ec8878125c573392ed20c9a5a1c0d2e25fee
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-03-09 01:17:47 +0000

    Change dir of some assets. Create app with slider function for buttons>

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
-const greet = ref("Hello World")
+
+const currentSlideIndex = ref(0)
+const currentSlide = computed(() => testimonialdata.value[currentSlideIndex.value])
+
+const previousSlide = () => {
+  currentSlideIndex.value = (currentSlideIndex.value - 1 + testimonialdata.value.length) % testimonialdata.value.length;
+};
+
+const nextSlide = () => {
+  currentSlideIndex.value = (currentSlideIndex.value + 1) % testimonialdata.value.length;
+}
+
 const testimonialdata = ref([{
   user_name: " John Tarkpor",
   user_role: "Junior Front-end Developer",
   "user_testimonial": " If you want to lay the best foundation possible I’d recommend taking this course.\
   The depth the instructors go into is incredible. I now feel so confident about \
-  starting up as a professional developer. "
+  starting up as a professional developer. ",
+  "imageUrl": "./images/image-john.jpg"
 },
 {
   user_name: "Tanya Sinclair",
   user_role: "UX Engineer",
   "user_testimonial": " I’ve been interested in coding for a while but never taken the jump, until now.\
   I couldn’t recommend this course enough. I’m now in the job of my dreams and so\
-  excited about the future."
+  excited about the future.",
+  "imageUrl": "./images/image-tanya.jpg"
 },])
 
 </script>
 
 <template>
-  <h1>{{ greet }}</h1>
+  <div>
+    <article>
+      <div class="image-container">
+        <img :src="currentSlide.imageUrl" :alt="currentSlide.user_name">
+        <div class="nav-buttons">
+          <span role="button" @click="previousSlide" aria-label="previous testimonial">
+            <img src="./assets/icon-prev.svg" alt="">
+          </span>
+          <span role="button" @click="nextSlide" aria-label="next testimonial">
+            <img src="./assets/icon-next.svg" alt="">
+          </span>
+        </div>
+      </div>
+      <div>
+        <p>{{ currentSlide.user_testimonial }}</p>
+        <h1>{{ currentSlide.user_name }}</h1>
+        <p>{{ currentSlide.user_role }}</p>
+      </div>
+
+    </article>
+
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+img {
+  max-width: 50%;
+  display: block;
+}
+
+.image-container {
+  position: relative;
+  background-image: url("./images/pattern-bg.svg");
+  background-repeat: no-repeat;
+}
+
+.nav-buttons {
+  position: absolute;
+  bottom: -1rem;
+}
+</style>

d6db70fc76f55a6bdb60e5206e6408a6f3f45b93
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-04-20 18:18:04 +0000

    Profile picture component

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+    imageurl: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <img :src="imageurl" :alt="`Picture of ${username}`" class="profile-picture" />
+</template>
+
+<style>
+.profile-picture {
+    border-radius: 1rem;
+    max-width: 90%;
+}
+</style>
